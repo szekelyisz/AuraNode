2f2e9f9d895fad17e8d71f0ed49b4eec583073d3
Szabolcs Szekelyi <cc@mail.3d.hu>
2020-01-30 23:12:31 -0500

    Fix indentation

--- lib/BleeperCustomInterface/BleeperCustomInterface.cpp
+++ lib/BleeperCustomInterface/BleeperCustomInterface.cpp
@@ -23,65 +23,66 @@ BleeperCustomInterface::BleeperCustomInterface(uint16_t port)
 }
 
 BleeperCustomInterface::~BleeperCustomInterface() {
-	// TODO Auto-generated destructor stub
+    // TODO Auto-generated destructor stub
 }
 
 void BleeperCustomInterface::init() {
-	addHandler(this);
+    addHandler(this);
 }
 
 bool BleeperCustomInterface::canHandle(HTTPMethod method, String uri) {
-	return ((method == HTTP_GET || method == HTTP_POST) &&
-			(uri.equals(mBaseUri)))
-		|| (method == HTTP_GET && uri.equals(mJSUri));
+    return ((method == HTTP_GET || method == HTTP_POST) &&
+            (uri.equals(mBaseUri)))
+        || (method == HTTP_GET && uri.equals(mJSUri));
 }
 
-bool BleeperCustomInterface::handle(ESP8266WebServer& server, HTTPMethod method, String uri) {
-	bool ret = false;
-	if(method == HTTP_GET && uri.equals(mJSUri)) {
-		send_P(200, "text/javascript", BleeperCustomInterface_js);
-		return true;
-	}
-	if(!mPreRequestHook(method)) return false;
-	switch(method) {
-	case HTTP_GET:
-		ret = handleGet();
-		break;
-	case HTTP_POST:
-		ret = handlePost();
-		break;
-	default:
-		break;
-	}
-	if(ret) mPostRequestHook(method);
-	return ret;
+bool BleeperCustomInterface::handle(ESP8266WebServer& server,
+        HTTPMethod method, String uri) {
+    bool ret = false;
+    if(method == HTTP_GET && uri.equals(mJSUri)) {
+        send_P(200, "text/javascript", BleeperCustomInterface_js);
+        return true;
+    }
+    if(!mPreRequestHook(method)) return false;
+    switch(method) {
+    case HTTP_GET:
+        ret = handleGet();
+        break;
+    case HTTP_POST:
+        ret = handlePost();
+        break;
+    default:
+        break;
+    }
+    if(ret) mPostRequestHook(method);
+    return ret;
 }
 
 bool BleeperCustomInterface::handleGet() {
-	auto conn = client();
-	DynamicJsonDocument doc(2048);
-	std::vector<String> strings =
-			Bleeper.configuration.getAsDictionary(false).toStrings();
-	for(size_t i = 0; i < strings.size() - 1;) {
-		doc[strings[i++]] = strings[i++];
-	}
-	setContentLength(measureJson(doc));
-	send(200, "text/json", "");
-	serializeJson(doc, conn);
-	return true;
+    auto conn = client();
+    DynamicJsonDocument doc(2048);
+    std::vector<String> strings =
+            Bleeper.configuration.getAsDictionary(false).toStrings();
+    for(size_t i = 0; i < strings.size() - 1;) {
+        doc[strings[i++]] = strings[i++];
+    }
+    setContentLength(measureJson(doc));
+    send(200, "text/json", "");
+    serializeJson(doc, conn);
+    return true;
 }
 
 bool BleeperCustomInterface::handlePost() {
-	ConfigurationDictionary d;
-	for(int i = 0; i != args(); i++) {
-		logger.logf("Updating '%s' = '%s'",
-				argName(i).c_str(), arg(i).c_str());
-		d[argName(i)] = arg(i);
-		Bleeper.configuration.setFromDictionary(d);
-	}
-	return true;
+    ConfigurationDictionary d;
+    for(int i = 0; i != args(); i++) {
+        logger.logf("Updating '%s' = '%s'",
+                argName(i).c_str(), arg(i).c_str());
+        d[argName(i)] = arg(i);
+        Bleeper.configuration.setFromDictionary(d);
+    }
+    return true;
 }
 
  void BleeperCustomInterface::handle() {
-	 handleClient();
+     handleClient();
 };
